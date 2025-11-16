daa244334fba1db595eae8712f9c58d4df582bca
JulesDevine <jules.desmet@student.howest.be>
2025-11-16 11:58:03 +0100

    detail pages fix

--- preact/preactTryout/src/pages/Article.jsx
+++ preact/preactTryout/src/pages/Article.jsx
@@ -1,12 +1,20 @@
-export default function Article({id, title, projectManager, areasOfExpertise,themes,kernwoorden,text}){
+import articles from '../data/articles.json'
+
+export default function Article({id}){
+    const article = articles.find(a => String(a.id) === String(id))
+    
+    if (!article) {
+        return <div>Article not found</div>
+    }
+
     return(
         <div>
-            <h2>{id} - {title}</h2>
-            <p>project manager <span style={{fontWeight:'bold'}}>{projectManager}</span></p>
-            <p>Areas Of Expertise <span style={{fontWeight:'bold'}}>{areasOfExpertise}</span></p>
-            <p>themes <span style={{fontWeight:'bold'}}>{themes}</span></p>
-            <p>kernwoorden <span style={{fontWeight:'bold'}}>{kernwoorden}</span></p>
-            <p>text <span style={{fontWeight:'bold'}}>{text}</span></p>
+            <h2>{article.id} - {article.title}</h2>
+            <p>project manager <span style={{fontWeight:'bold'}}>{article.projectManager}</span></p>
+            <p>Areas Of Expertise <span style={{fontWeight:'bold'}}>{article.areasOfExpertise}</span></p>
+            <p>themes <span style={{fontWeight:'bold'}}>{article.themes}</span></p>
+            <p>kernwoorden <span style={{fontWeight:'bold'}}>{article.kernwoorden}</span></p>
+            <p>text <span style={{fontWeight:'bold'}}>{article.text}</span></p>
 
 
         </div>
