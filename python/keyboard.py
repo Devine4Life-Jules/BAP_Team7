e98b4c8629dc6b6764a8f3332cd8a912d2b2a7ce
JulesDevine <jules.desmet@student.howest.be>
2025-11-17 18:48:35 +0100

    update keyboard simulation

--- python/keyboard.py
+++ python/keyboard.py
@@ -2,7 +2,21 @@ import RPi.GPIO as GPIO
 import time
 import uinput
 
-BUTTON_PIN = 2
+BUTTON_PIN = 2  
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+device = uinput.Device([uinput.KEY_SPACE])
+
+print("Ready! Press the button to output SPACE.")
+
+try:
+    while True:
+        if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+            device.emit_click(uinput.KEY_SPACE)
+            print("SPACE pressed")
+            time.sleep(0.25)  # debounce
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    GPIO.cleanup()
