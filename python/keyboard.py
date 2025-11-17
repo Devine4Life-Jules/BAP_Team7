2954323f47869201ce7e44bb47709e9c208ef3e7
jules <jules.desmet@student.howest.be>
2025-11-17 20:06:25 +0100

    added space and backspace button presses

--- python/keyboard.py
+++ python/keyboard.py
@@ -2,21 +2,30 @@ import RPi.GPIO as GPIO
 import time
 import uinput
 
-BUTTON_PIN = 2  
+SPACE_BUTTON= 2
+BACKSPACE_BUTTON= 3
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SPACE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BACKSPACE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-device = uinput.Device([uinput.KEY_SPACE])
+device = uinput.Device([uinput.KEY_SPACE, uinput.KEY_BACKSPACE ])
 
 print("Ready! Press the button to output SPACE.")
 
 try:
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+        if GPIO.input(SPACE_BUTTON) == GPIO.LOW:
             device.emit_click(uinput.KEY_SPACE)
             print("SPACE pressed")
-            time.sleep(0.25)  # debounce
+            time.sleep(0.25)  
+
+        if GPIO.input(BACKSPACE_BUTTON) == GPIO.LOW:
+            device.emit_click(uinput.KEY_BACKSPACE)
+            print("BACKSPACE pressed") 
+            time.sleep(0.25)  
+
+
         time.sleep(0.01)
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    GPIO.cleanup()    
