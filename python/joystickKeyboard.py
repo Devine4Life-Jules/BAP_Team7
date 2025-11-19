c33d4b4409668c89bdd03289f308474349f6c03c
JulesDevine <jules.desmet@student.howest.be>
2025-11-19 22:14:42 +0100

    uinput for knob and joystick

--- python/joystickKeyboard.py
+++ python/joystickKeyboard.py
@@ -1,114 +1,104 @@
 #!/usr/bin/env python3
+import RPi.GPIO as GPIO
 import spidev
 import time
-from pynput.keyboard import Controller, Key
-import RPi.GPIO as GPIO
+import uinput
 
-# Setup keyboard controller
-keyboard = Controller()
-
-# Open SPI bus for joystick
+# Setup SPI for joystick
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1350000
 
 # Rotary encoder pins
-CLK_PIN = 17  # Change these to whatever GPIO pins you want to use
+CLK_PIN = 17
 DT_PIN = 18
 
-# Setup GPIO for rotary encoder
+# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CLK_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(DT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Track rotary encoder state
 clk_last = GPIO.input(CLK_PIN)
+
+# Setup uinput virtual keyboard
+device = uinput.Device([
+    uinput.KEY_UP,
+    uinput.KEY_DOWN,
+    uinput.KEY_LEFT,
+    uinput.KEY_RIGHT,
+    uinput.KEY_Q,  # Rotary left
+    uinput.KEY_E   # Rotary right
+])
 
 def read_adc(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
-# Joystick channels
-vrx_channel = 1  # X-axis
-vry_channel = 2  # Y-axis
-
-# Thresholds for joystick
+# Joystick settings
+vrx_channel = 1
+vry_channel = 2
 CENTER = 512
 DEADZONE = 100
 THRESHOLD_LOW = CENTER - DEADZONE
 THRESHOLD_HIGH = CENTER + DEADZONE
 
-# Track previous joystick state
 last_direction = None
 
-# Keys for rotary encoder
-ROTARY_LEFT_KEY = 'q'   # Change to whatever key you want for counter-clockwise
-ROTARY_RIGHT_KEY = 'e'  # Change to whatever key you want for clockwise
-
-print("Joystick + Rotary Encoder - Press CTRL+C to exit")
-print("=" * 50)
-print("Joystick: Arrow keys")
-print(f"Rotary Left: {ROTARY_LEFT_KEY}")
-print(f"Rotary Right: {ROTARY_RIGHT_KEY}")
+print("Joystick + Rotary Encoder Ready!")
 print("=" * 50)
 
 try:
     while True:
-        # === READ ROTARY ENCODER ===
+        # === ROTARY ENCODER ===
         clk_state = GPIO.input(CLK_PIN)
         dt_state = GPIO.input(DT_PIN)
         
         if clk_state != clk_last:
             if dt_state != clk_state:
-                # Clockwise
-                keyboard.press(ROTARY_RIGHT_KEY)
-                keyboard.release(ROTARY_RIGHT_KEY)
-                print(f"Rotary: Clockwise ({ROTARY_RIGHT_KEY})")
+                device.emit_click(uinput.KEY_E)
+                print("Rotary: Right (E)")
             else:
-                # Counter-clockwise
-                keyboard.press(ROTARY_LEFT_KEY)
-                keyboard.release(ROTARY_LEFT_KEY)
-                print(f"Rotary: Counter-clockwise ({ROTARY_LEFT_KEY})")
+                device.emit_click(uinput.KEY_Q)
+                print("Rotary: Left (Q)")
         
         clk_last = clk_state
         
-        # === READ JOYSTICK ===
+        # === JOYSTICK ===
         x_value = read_adc(vrx_channel)
         y_value = read_adc(vry_channel)
         
         current_direction = None
-        
-        # Determine direction
         x_diff = abs(x_value - CENTER)
         y_diff = abs(y_value - CENTER)
         
         if x_diff > y_diff:
             if x_value < THRESHOLD_LOW:
-                current_direction = Key.left
+                current_direction = uinput.KEY_LEFT
             elif x_value > THRESHOLD_HIGH:
-                current_direction = Key.right
+                current_direction = uinput.KEY_RIGHT
         else:
             if y_value < THRESHOLD_LOW:
-                current_direction = Key.down
+                current_direction = uinput.KEY_DOWN
             elif y_value > THRESHOLD_HIGH:
-                current_direction = Key.up
+                current_direction = uinput.KEY_UP
         
-        # Only send keypress if direction changed
         if current_direction != last_direction:
             if current_direction:
-                keyboard.press(current_direction)
-                print(f"Joystick: {current_direction}")
-                time.sleep(0.05)
-                keyboard.release(current_direction)
+                device.emit_click(current_direction)
+                key_name = {
+                    uinput.KEY_UP: "UP",
+                    uinput.KEY_DOWN: "DOWN",
+                    uinput.KEY_LEFT: "LEFT",
+                    uinput.KEY_RIGHT: "RIGHT"
+                }.get(current_direction, "")
+                print(f"Joystick: {key_name}")
             
             last_direction = current_direction
         
-        time.sleep(0.001)  # Fast polling for rotary encoder
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
-    print("\n\nExiting...")
+    print("\nExiting...")
     GPIO.cleanup()
     spi.close()
-
-
