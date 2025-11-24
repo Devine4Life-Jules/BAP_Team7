ae9f3b1796063da3f34b1e881b83d511929dd262
JulesDevine <jules.desmet@student.howest.be>
2025-11-24 20:15:55 +0100

    updated some bgColors

--- preact/Orbit/src/components/Planet.jsx
+++ preact/Orbit/src/components/Planet.jsx
@@ -13,7 +13,7 @@ export default function Planet({id,bgColor, title}) {
     return(
         <>
             <Link href={`/project/${id}`} style={TextStyle}>
-                <div id={id} style={{width:'100px',height:"100px",borderRadius:"50%",background:bgColor, position:"absolute"}}>
+                <div id={id} style={{width:'100px',height:"100px",borderRadius:"50%",background:bgColor, position:"absolute", border: '2px solid #EBF6F9'}}>
                     <h3>{title}</h3>
                 </div>
             </Link>
