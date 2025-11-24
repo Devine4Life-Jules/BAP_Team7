5f4940d5872105754631a5251c7c4d71e5e176a1
JulesDevine <jules.desmet@student.howest.be>
2025-11-24 20:00:35 +0100

    make titles more readable

--- preact/Orbit/src/components/Planet.jsx
+++ preact/Orbit/src/components/Planet.jsx
@@ -1,10 +1,19 @@
 import { Link } from "preact-router"
 
 export default function Planet({id,bgColor, title}) {
+
+    const TextStyle = {
+        textDecoration: 'none',
+        color: 'white',
+        textShadow: '0 0 5px purple',
+        textAlign: 'center',
+        backgroundColor:'red',
+    };
+
     return(
         <>
-            <Link href={`/project/${id}`}>
-                <div id={id} style={{width:'100px',height:"100px",borderRadius:"50%",backgroundColor:bgColor, position:"absolute"}}>
+            <Link href={`/project/${id}`} style={TextStyle}>
+                <div id={id} style={{width:'100px',height:"100px",borderRadius:"50%",background:bgColor, position:"absolute"}}>
                     <h3>{title}</h3>
                 </div>
             </Link>
