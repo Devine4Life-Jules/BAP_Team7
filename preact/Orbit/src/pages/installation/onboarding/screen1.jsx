203e845d670c63476fc5ef8df3b0e51e7cb9f06f
JulesDevine <jules.desmet@student.howest.be>
2025-11-27 13:40:05 +0100

    added more content to onboarding

--- preact/Orbit/src/pages/installation/onboarding/screen1.jsx
+++ preact/Orbit/src/pages/installation/onboarding/screen1.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'preact/hooks';
 import { route } from 'preact-router';
 import Button from '../../../components/Button';
+import logoHowestResearch from '../../../assets/logo_howestResearch.png'
 
 export default function Screen1() {
   useEffect(() => {
@@ -19,8 +20,12 @@ export default function Screen1() {
   return (
     <div className='onboarding-screen' >
       <h1>ORBIT</h1>
-      <p>Lanceer je bedrijf naar de sterren met Howest Research</p>
+      <p>Lanceer je bedrijf naar de sterren</p>
+      <div style={{width: "50%"}}>
+        <img src={logoHowestResearch} alt="Howest Research Logo" />
+      </div>
       <Button type="check" text="Start"></Button>
+
     </div>
   );
 }
--- preact/Orbit/src/pages/installation/onboarding/screen2.jsx
+++ preact/Orbit/src/pages/installation/onboarding/screen2.jsx
@@ -2,6 +2,8 @@
 import { useEffect } from "preact/hooks";
 import { route } from 'preact-router';
 import Button from '../../../components/Button';
+import logoHowestResearch from '../../../assets/logo_howestResearch.png'
+
 
 function Screen2 () {
     useEffect(() => {
@@ -21,7 +23,11 @@ function Screen2 () {
     },);  
     return(
         <div className="onboarding-screen">
-            <h2>Wie is Howest Research?</h2>
+            <h2>Wie is Howest</h2>
+            <div style={{width: "50%"}}>
+              <img src={logoHowestResearch} alt="Howest Research Logo" />
+            </div>
+            <p>Innovatief en missiegedreven onderzoek met impact in vijf domeinen</p>
             <Button icon="back" text="back"></Button>
             <Button icon="check" text="Next"></Button>
         </div>
