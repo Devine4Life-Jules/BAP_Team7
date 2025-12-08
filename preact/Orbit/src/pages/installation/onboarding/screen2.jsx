89230fd9648cd7fa6a84aeaac3b38e191a0c7c9f
JulesDevine <jules.desmet@student.howest.be>
2025-12-08 18:16:30 +0100

    added backgorund screen2

--- preact/Orbit/src/pages/installation/onboarding/screen2.jsx
+++ preact/Orbit/src/pages/installation/onboarding/screen2.jsx
@@ -7,12 +7,12 @@ function Screen2 () {
     useKeyboardNavigation({back: '/', next: '/screen3'});
     useReBoot({rebootTime: 100000});
     return(
-        <div className="onboarding-screen">
+        <div className="onboarding-screen screen2">
             <h2>Wie is Howest</h2>
             <div style={{width: "50%"}}>
               <img src={logoHowestResearch} alt="Howest Research Logo" />
             </div>
-            <p>Innovatief en missiegedreven onderzoek met impact in vijf domeinen</p>
+            <p>Missiegedreven onderzoek dat jouw bedrijf lanceert naar nieuwe hoogtes</p>
             <div style={{display: 'flex', gap: '20px'}}>
               <Button icon="back" text="back"></Button>
               <Button icon="check" text="Next"></Button>
