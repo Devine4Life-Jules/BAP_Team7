e38d2edce59ab87d7e1e9d8c7dd51d768750ab85
JulesDevine <jules.desmet@student.howest.be>
2025-12-15 09:55:52 +0100

    feat: integrate PhoneKeywordPills component into PhoneCard for enhanced keyword display

--- preact/Orbit/src/components/KeyWordsPills.jsx
+++ preact/Orbit/src/components/KeyWordsPills.jsx
@@ -1,8 +1,8 @@
-export default function KeyWordPills({keywords, pillClassName}){
+export default function KeyWordPills({keywords = [], pillClassName, wrapperClassName}){
     return (
         <>
             {keywords.length > 0 && (
-                <div style={{ display: 'flex', flexWrap: 'wrap', gap: '8px', marginTop: '10px', marginBottom: '20px'  }}>
+                <div className={wrapperClassName} style={{ display: 'flex', flexWrap: 'wrap', gap: '8px', marginTop: '10px', marginBottom: '20px'  }}>
                     {keywords.map((keyword, index) => (
                         <span key={index} className={pillClassName}>
                             {keyword.label}
--- preact/Orbit/src/components/PhoneCard.jsx
+++ preact/Orbit/src/components/PhoneCard.jsx
@@ -2,10 +2,14 @@ import { Link } from 'preact-router'
 import dummyImage from '../assets/dummyImage.png';
 import useGetDomains from '../hooks/useGetDomains';
 import PhoneDomainPills from './PhoneDomainPills';
+import useGetKeywords from '../hooks/useGetKeywords';
+import PhoneKeywordPills from '../components/KeyWordsPills';
 
 
 
 export default function PhoneCard({ project, background, textColor }) {
+
+        const keywords = useGetKeywords(project);
 
             const cardStyle = {
             border: `1px solid ${textColor || '#ccc'}`,
@@ -44,7 +48,23 @@ export default function PhoneCard({ project, background, textColor }) {
             }}
         >
             <div style={cardStyle} >
-                <img src={dummyImage} alt="dummy image" />
+                <div style={{position: 'relative', height: '150px', width: '100%', overflow: 'hidden'}}>
+                    <div style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundImage:`url(${project.image || dummyImage})`, 
+                        backgroundSize: 'cover', 
+                        backgroundPosition: 'center',
+                        filter: "brightness(0.8)",
+                        zIndex: 0
+                    }}></div>
+                    <div style={{position: 'relative', zIndex: 1}}>
+                        <PhoneKeywordPills keywords={keywords} pillClassName="keyword-pill-card" wrapperClassName="keyword-wrapper-card" />
+                    </div>
+                </div>
                 <div style={cardLabelStyle}>
                 <div>
                     <h2 style={cardTitleStyle}>
